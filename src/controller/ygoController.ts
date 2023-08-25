d5d897aac309dbe54b51a7d65715b515f24732bd
godley0623 <godley11691@yahoo.com>
2023-08-25 18:54:30 -0400

    worked on info-page

--- src/controller/ygoController.ts
+++ src/controller/ygoController.ts
@@ -1,6 +1,37 @@
 import cardDB from '../data/cardDB.json'
 import { CardType } from '../types/cardType'
 
+export const levelImages = {
+    level: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/icon_level.png',
+    rank: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/icon_rank.png',
+    link: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/link_pc/link7913.png',
+    pscale: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/icon_pendulum.png'
+}
+
+export function attributeImages(attribute:string) {
+    const attObj:any = {
+        fire: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/attribute/attribute_icon_fire.png',
+        wind: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/attribute/attribute_icon_wind.png',
+        earth: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/attribute/attribute_icon_earth.png',
+        water: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/attribute/attribute_icon_water.png',
+        light: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/attribute/attribute_icon_light.png',
+        dark: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/attribute/attribute_icon_dark.png',
+        divine: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/attribute/attribute_icon_divine.png'
+    }
+    return attObj[attribute]
+}
+
+export const stImages = {
+    spell: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/attribute/attribute_icon_spell.png',
+    trap: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/attribute/attribute_icon_trap.png',
+    equip: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/effect/effect_icon_equip.png',
+    field: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/effect/effect_icon_field.png',
+    quickPlay: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/effect/effect_icon_quickplay.png',
+    ritual: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/effect/effect_icon_ritual.png',
+    continuous: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/effect/effect_icon_continuous.png',
+    counter: 'https://www.db.yugioh-card.com/yugiohdb/external/image/parts/effect/effect_icon_counter.png'
+}
+
 export function getRandomCards(amount: number): CardType[] {
     const randomCards: CardType[] = []
     const cards = cardDB as []
--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { getCardsById } from '../controller/ygoController'
+import { getCardsById, levelImages, attributeImages } from '../controller/ygoController'
 
 export default function InfoPage() {
     const cardId = Number(useParams().id)
@@ -10,6 +10,20 @@ export default function InfoPage() {
     <div className='info-page'>
         <div className='info-container'>
             <img src={card.image} alt={`${card.name}`}/>
+            <h4>{card.name}</h4>
+            <div className='lvl-attribute'>
+                {card.frame_type !== 'xyz' && card.frame_type !== 'link' && card.frame_type !== 'spell' && card.frame_type !== 'trap' &&
+                <>
+                <div className='levels'>
+                    <img src={levelImages['level']} alt='level images'/>
+                    <h4>x{card.level}</h4>
+                </div>
+                <div className='attribute'>
+                    <img src={attributeImages(card.attribute.toLowerCase())}/>
+                </div>
+                </>
+                }
+            </div>
         </div>
     </div>
   )
