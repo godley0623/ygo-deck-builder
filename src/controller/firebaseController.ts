420cb725eebb5da82861220c9d4dd9bc2ef7b722
godley0623 <godley11691@yahoo.com>
2023-08-26 17:57:21 -0400

    commented out firebase controller

--- src/controller/firebaseController.ts
+++ src/controller/firebaseController.ts
@@ -1,42 +1,42 @@
-import { initializeApp } from 'firebase/app'
-import { query, where, onSnapshot } from 'firebase/firestore'
-import { firebaseConfig, colRef } from '../firebase/firebaseConfig'
-import { CardType } from '../types/cardType'
-import { shuffleArray } from './controller'
+// import { initializeApp } from 'firebase/app'
+// import { query, where, onSnapshot } from 'firebase/firestore'
+// import { firebaseConfig, colRef } from '../firebase/firebaseConfig'
+// import { CardType } from '../types/cardType'
+// import { shuffleArray } from './controller'
 
-initializeApp(firebaseConfig)
+// initializeApp(firebaseConfig)
 
-export function getRandomCards(amount: number) {
-    const q = query(colRef)
-    const cards:CardType[] = []
-    const result:unknown[] = []
+// export function getRandomCards(amount: number) {
+//     const q = query(colRef)
+//     const cards:CardType[] = []
+//     const result:unknown[] = []
 
-    onSnapshot(q, (snapshot) => {
-        snapshot.docs.forEach((doc) => {
-            const cardData = doc.data() as CardType;
-            cards.push({ ...cardData })
-        })
+//     onSnapshot(q, (snapshot) => {
+//         snapshot.docs.forEach((doc) => {
+//             const cardData = doc.data() as CardType;
+//             cards.push({ ...cardData })
+//         })
 
-        console.log(cards.length)
-        // for (let i=0; i<amount; i++) {
-        //     let randomIndex = Math.floor(Math.random() * cards.length)
-        //     result.push(cards[randomIndex]) 
-        // }
+//         console.log(cards.length)
+//         // for (let i=0; i<amount; i++) {
+//         //     let randomIndex = Math.floor(Math.random() * cards.length)
+//         //     result.push(cards[randomIndex]) 
+//         // }
 
-        return result
-    })
-}
+//         return result
+//     })
+// }
 
-export function getCardsByArchetype(archetype: string) {
-    const q = query(colRef, where("archetype", "==", archetype))
-    const cards:CardType[] = []
+// export function getCardsByArchetype(archetype: string) {
+//     const q = query(colRef, where("archetype", "==", archetype))
+//     const cards:CardType[] = []
 
-    onSnapshot(q, (snapshot) => {
-        snapshot.docs.forEach((doc) => {
-            const cardData = doc.data() as CardType;
-            cards.push({ ...cardData })
-        })
-    })
+//     onSnapshot(q, (snapshot) => {
+//         snapshot.docs.forEach((doc) => {
+//             const cardData = doc.data() as CardType;
+//             cards.push({ ...cardData })
+//         })
+//     })
 
-    return cards
-}
+//     return cards
+// }
